db38c0c1feb01c40932f1f349364bd33c807a170
hrsprs <jamescharleson@yahoo.com>
2022-11-30 19:49:24 -0700

    added schema tables

---
+++ db/seeds.sql
@@ -0,0 +1,39 @@
+
+-- if the database exists, following code will drop it and create
+DROP DATABASE IF EXISTS team_db;
+CREATE DATABASE team_db;
+
+USE team_db;
+
+-- Creating schema table for differnt deparments of employees
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+
+-- Creating schema table for roles of employee
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    deparment_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+
+-- Creating schema table for employee
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
